13e14b819286646a573d5aaf5ad6e865cbc5c9e5
Baran Kazan <Baran.kazan@hotmail.com>
2022-02-28 00:33:09 +0100

    Shows parameters that is required to use the command

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ import os
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_choice, create_option
+from discord_slash.utils.manage_commands import create_option, create_choice
 
 client = commands.Bot(command_prefix="/")
 slash = SlashCommand(client, sync_commands=True)
+
 
 @client.event
 async def on_ready():
     print(f"{client.user} is initiated!")
 
-@slash.slash(name="hello",
-             description="Sends Hello message!")
-async def _hello(ctx:SlashContext):
-    await ctx.send("Hello!")
+
+@slash.slash(name="get_role",
+             description="Get your raid role by typing in your Bungie id",
+             options=[
+                 create_option(
+                     name="username",
+                     description="Type in your Bungie username, including the numbers after #",
+                     required=True,
+                     option_type=3
+                 )
+             ]
+             )
+async def _get_role(ctx: SlashContext, username: str):
+    await ctx.send(username)
+
 
 client.run(os.getenv("TOKEN"))
