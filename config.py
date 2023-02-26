f42561e0b2194ad87d0a9b7ccd2f803b62b0a9f0
Baran Kazan <Baran.kazan@hotmail.com>
2023-02-26 20:29:47 +0100

    Init

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# Needs be changed on release
+GUILD_ID = 622450248362754050
+BUNGIE_TOKEN = os.getenv("BUNGIE_TOKEN")
--- main.py
+++ main.py
@@ -1,32 +1,15 @@
+import interactions
+from config import *
 import os
 
-import discord
-from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_option, create_choice
+bot = interactions.Client(token=DISCORD_TOKEN)
 
-client = commands.Bot(command_prefix="/")
-slash = SlashCommand(client, sync_commands=True)
+@bot.command(
+    name="my_first_command",
+    description="This is the first command I made!",
+    scope=GUILD_ID,
+)
+async def my_first_command(ctx: interactions.CommandContext):
+    await ctx.send("Hi there!")
 
-
-@client.event
-async def on_ready():
-    print(f"{client.user} is initiated!")
-
-
-@slash.slash(name="get_role",
-             description="Get your raid role by typing in your Bungie id",
-             options=[
-                 create_option(
-                     name="username",
-                     description="Type in your Bungie username, including the numbers after #",
-                     required=True,
-                     option_type=3
-                 )
-             ]
-             )
-async def _get_role(ctx: SlashContext, username: str):
-    await ctx.send(username)
-
-
-client.run(os.getenv("TOKEN"))
+bot.start()
