b7985ab0f0dfc2a9e9bfd3a87e1d5a46ee8753df
Baran Kazan <Baran.kazan@hotmail.com>
2023-04-10 17:47:01 +0200

    The environment variables now read from a file

--- resources/config.py
+++ resources/config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+#Load the enviorment variables below
+load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 # Needs be changed on release
 GUILD_ID = os.getenv("GUILD_ID")
-BUNGIE_TOKEN = os.getenv("BUNGIE_TOKEN")
+BUNGIE_TOKEN = os.getenv("  BUNGIE_TOKEN")
